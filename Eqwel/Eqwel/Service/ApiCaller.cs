289211e5e72b335456e11ea67fd80e7026c1d507
kirill13079 <kirillzhenkevich13@gmail.com>
2022-07-12 10:15:37 +0300

    update

---
+++ Eqwel/Eqwel/Service/ApiCaller.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Eqwel.Service
+{
+    public class ApiCaller
+    {
+        public static async Task<string> Get(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                var request = await client.GetAsync(url);
+
+                return request.IsSuccessStatusCode ? await request.Content.ReadAsStringAsync() : null;
+            }
+        }
+    }
+}
--- Eqwel/Eqwel/ViewModels/MainViewModel.cs
+++ Eqwel/Eqwel/ViewModels/MainViewModel.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using Eqwel.Service;
+using MvvmHelpers;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Eqwel.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : BaseViewModel
     {
+        private readonly ApiCaller _apiCaller = new ApiCaller();
+
+        public MainViewModel()
+        { 
+        
+        }
     }
 }
